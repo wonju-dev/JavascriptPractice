dfa87cc7665929e8a139710fcc5f566969bae641
wonju-dev <wonjulee.dev@gmail.com>
2021-11-06 23:44:37 +0900

    reactType : recoil selectorFamily

--- reactType/test/src/components/Post.tsx
+++ reactType/test/src/components/Post.tsx
@@ -3,6 +3,8 @@ import { useRecoilValue } from "recoil";
 import { withPostList } from "../recoil/post";
 
 export const Post = () => {
-  const postList = useRecoilValue(withPostList);
-  return <ul></ul>;
+  // const postList = useRecoilValue(withPostList("dummy.json"));
+  // console.log(postList);
+  // const li = postList.map((value, index) => <li key={index}>{value}</li>);
+  return <ul>hihi</ul>;
 };
--- reactType/test/src/recoil/post/withPostList.tsx
+++ reactType/test/src/recoil/post/withPostList.tsx
@@ -1,12 +1,14 @@
 import { selectorFamily } from "recoil";
 import { clickedMenuAtom } from "../header";
 
-export const withPostList = selectorFamily<Promise<string>>({
+export const withPostList = selectorFamily<Promise<string>, string>({
   key: "withPostList",
-  get: async (get) => {
-    const filter = get(clickedMenuAtom);
-    const response = await fetch("post.json");
-    const json = await response.json();
-    return json[filter];
-  },
+  get:
+    (url) =>
+    async ({ get }) => {
+      const filter = get(clickedMenuAtom);
+      const response = await fetch(url);
+      const json = await response.json();
+      return json[filter];
+    },
 });
